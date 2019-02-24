c2422b10a4956e6824689dd4a87387bff1a926de
Eric Zhang <eric@wirelessprv-10-195-55-36.near.illinois.edu>
2019-02-23 21:00:39 -0600

    ReceiptParser

--- app/src/main/java/com/example/jho/hackillinois/MainActivity.java
+++ app/src/main/java/com/example/jho/hackillinois/MainActivity.java
@@ -1,13 +1,7 @@
 package com.example.jho.hackillinois;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 }
---
+++ app/src/main/java/com/example/jho/hackillinois/ReceiptParser.java
@@ -0,0 +1,12 @@
+package com.example.jho.hackillinois;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.HashMap;
+
+public class ReceiptParser {
+    public HashMap<String, Integer> Parse(){
+
+    }
+
+}
