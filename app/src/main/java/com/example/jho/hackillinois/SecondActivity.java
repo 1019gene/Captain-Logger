693a559b9e10b425c674f502b2ad7e2fab1a4007
peterchien <peterc3@illinois.edu>
2019-02-23 21:37:51 -0600

    created basic UI flow

--- app/src/main/java/com/example/jho/hackillinois/SecondActivity.java
+++ app/src/main/java/com/example/jho/hackillinois/SecondActivity.java
@@ -1,13 +1,24 @@
 package com.example.jho.hackillinois;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
-    @Override
+    Button manually = findViewById(R.id.manually_btn);
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        manually.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SecondActivity.this, ThirdActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/jho/hackillinois/ThirdActivity.java
+++ app/src/main/java/com/example/jho/hackillinois/ThirdActivity.java
@@ -1,13 +1,24 @@
 package com.example.jho.hackillinois;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ThirdActivity extends AppCompatActivity {
 
-    @Override
+    Button done = findViewById(R.id.done_btn);
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ThirdActivity.this, SecondActivity.class));
+            }
+        });
     }
 }
