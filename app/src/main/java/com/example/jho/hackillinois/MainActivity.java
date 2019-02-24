b50b6d91476b63766d3a1a57ff3359699b5b13bf
peterchien <peterc3@illinois.edu>
2019-02-23 23:01:24 -0600

    fixed everything now works

--- app/src/main/java/com/example/jho/hackillinois/MainActivity.java
+++ app/src/main/java/com/example/jho/hackillinois/MainActivity.java
@@ -1,9 +1,32 @@
 package com.example.jho.hackillinois;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText editTextUsername, editTextPassword;
+
+    private FirebaseAuth fire;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
 
         FirebaseApp.initializeApp(MainActivity.this);
         fire = FirebaseAuth.getInstance();
-    }    
+    }
 
     private void userLogin() {
         String username = editTextUsername.getText().toString().trim();
